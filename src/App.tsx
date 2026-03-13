887a15c1a79c8f8d816c8e843ee77f266c6bc161
nitinv345 <nobitav345@gmail.com>
2026-03-13 07:09:15 +0530

    chore: sync with github and final configuration cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -6,7 +6,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { AuthProvider } from './context/AuthContext';
 import { CartProvider } from './context/CartContext';
-import Navbar from './components/Navbar';
 import Layout from './components/Layout';
 import Home from './pages/Home';
 import Login from './pages/Login';
